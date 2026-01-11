6b336f7ad8c8e6b51eaaea8e7161c9cf61968882
Onyex15 <onyekachikingsley15@gmail.noreply.github.com>
2026-01-11 10:32:57 +0100

    Continuation of my dev journey

---
+++ Onyex projects/JS practice/day1/day1_variables.js
@@ -0,0 +1,13 @@
+const name = prompt("What is your name?");
+const age = prompt("How old are you?");
+const color = prompt("What is your favorite color?");
+const hobby = prompt("What is your hobby?");
+
+
+const nextAge = Number(age) + 1;
+
+console.log(`Hey ${name} !`);
+console.log(`You're ${age} years old.`);
+console.log(`Your favourite color is  ${color}`);
+console.log(`Your hobby is  ${hobby}`);
+console.log(`You'll be  ${nextAge}  next year.`);
---
+++ Onyex projects/JS practice/day1/mad_libs_game.js
@@ -0,0 +1,6 @@
+const name = prompt("What is your name?");
+const age =Number(prompt("How old are you?"));
+const color = prompt("What is your favorite color");
+const myAge = age + 1;
+
+console.log( ` Hey   ${name}   I heard you are   ${age}   and your favorite color is   ${color}   . You will probably be   ${myAge}   next year  ` );
